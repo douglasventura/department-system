b1046d053543a80c56332f7ec66c06a62d712535
Douglas Ventura <douglasventura.pe@gmail.com>
2022-08-24 20:59:32 -0300

    Seller entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,17 @@
 package application;
 
+import java.time.Instant;
+
 import modelentities.Department;
+import modelentities.Seller;
 
 public class App {
     public static void main(String[] args) {
+        
         Department d1 = new Department(1, "Books");
-        System.out.println(d1);
+        
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", Instant.now(), 3000.0, d1);
+
+        System.out.println(seller);
     }
 }
