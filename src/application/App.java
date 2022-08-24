8848b55403158cf1b7c3d67f3bac1d64eabb472e
Douglas Ventura <douglasventura.pe@gmail.com>
2022-08-24 20:42:58 -0300

    Department entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,10 @@
 package application;
 
+import modelentities.Department;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Department d1 = new Department(1, "Books");
+        System.out.println(d1);
     }
 }
