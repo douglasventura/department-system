3661a5f67c61bad56935391666c0967f682a65ce
Douglas Ventura <douglasventura.pe@gmail.com>
2022-08-24 22:42:24 -0300

    findById implementation

--- src/application/App.java
+++ src/application/App.java
@@ -1,20 +1,14 @@
 package application;
 
-import java.time.Instant;
-
 import modeldao.DaoFactory;
 import modeldao.SellerDao;
-import modelentities.Department;
 import modelentities.Seller;
 
 public class App {
     public static void main(String[] args) {
         
-        Department d1 = new Department(1, "Books");
-        
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", Instant.now(), 3000.0, d1);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
--- src/modeldao/DaoFactory.java
+++ src/modeldao/DaoFactory.java
@@ -1,10 +1,11 @@
 package modeldao;
 
+import db.DB;
 import modeldaoimpl.SellerDaoJDBC;
 
 public class DaoFactory {
     
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
